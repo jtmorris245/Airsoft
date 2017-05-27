2120bf1125d222240fdea90b72b166b85a20285c
Joseph Morris <jtm245@gmail.com>
2017-05-27 22:36:09 +0100

    Added Display Folder and Worked on main parser and generator.

--- MainUnit/main/main.ino
+++ MainUnit/main/main.ino
@@ -5,7 +5,7 @@ void setup() {
   Serial.begin(9600);
   rfSerial.begin(1200);
 }
-
+int state[4];
 void loop() {
   // put your main code here, to run repeatedly:
   int addr; //addr to send to ( 0-4 ) 
@@ -13,8 +13,8 @@ void loop() {
   //For Testing...
   Serial.println("Polling Each Address for Current Score...");
   type_flag=false; //Request
-  int[4] scoreR;
-  int[4] scoreB;
+  int ScoreR[4];
+  int scoreB[4];
   for(addr=0;addr<4;addr++)
   {
     String Packet = createPacket(addr,type_flag,byte(1));
@@ -30,6 +30,49 @@ void loop() {
         rec=true;
         break;
       }
+      delay(1000);
+    }
+    if(!rec)
+    {
+      Serial.println("No Packet recieved in 5 Seconds from Addr #" + addr);
+      state[addr]=0;
+      continue;
+    }
+    else
+    {
+      
+      delay(50);
+      String RecP = String(rfSerial.read());
+      state[addr]=0;
+      int Res[3];
+      parsePacket(RecP,0,Res);
+      //scoreR[addr]=rec;
+      //scoreB[addr]=rec;
+    }
+  }
+  //TODO: Display Update.
+  //TODO: Check win condition.
+  //TODO: Do Horn Blasts.
+  //TODO: Reset Condition
+  //Reset all Slaves
+  addr=0;
+  type_flag=true; //Command
+  byte cmd = byte('R'); // R for Reset
+  for(addr=0;addr<4;addr++)
+  {
+    String Packet = createPacket(addr,type_flag,cmd);
+    Serial.println("Sending Reset Command Packet:"+Packet);
+    rfSerial.println(Packet);
+    int i =0;
+    bool rec = false;
+    for(i=0;i<5;i++)
+    {
+      if(rfSerial.available())
+      {
+        rec=true;
+        break;
+      }
+      delay(1000);
     }
     if(!rec)
     {
@@ -38,15 +81,40 @@ void loop() {
     }
     else
     {
-      delay(250);
+      delay(50);
       String RecP = String(rfSerial.read());
-      // Plug in the parser here/
-      //scoreR[addr]=rec;
-      //scoreB[addr]=rec;
+      state[addr]=0;
+      int Res[3];
+      parsePacket(RecP,0,Res);
+      
+
     }
+    
   }
-  //TODO: Display Update.
-  //TODO: Check win condition.
+  
+  
+}
+
+
+void parsePacket(String packet,int addr_cur,int *Res)
+{
+  int addr = packet.substring(0,1).toInt();
+  if(addr_cur != addr)
+  { //Packet isn't for us, return null
+    return NULL;
+  }
+  bool type = packet.substring(1,2)=="R" ? false : true;
+  byte Resp =0;
+  if(packet.length() > 3)
+  {
+    byte Resp = NULL;
+    int len = packet.length() - 2;
+    packet.substring(2,3).getBytes(Resp,len);
+  }
+  Res[0]=addr;
+  Res[1]=(int)type;
+  Res[2]=(int)Resp;
+  
 }
 
 
@@ -58,4 +126,29 @@ String createPacket(int addr, bool type, byte response) // Other flags ignored f
   packet.concat(String(int(response)));
   return packet;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
