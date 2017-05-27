e932f1bcd5adc6e0becac488459943f362e6503b
Joseph Morris <jtm245@gmail.com>
2017-05-27 19:16:59 +0100

    Added Test cases to main.ino

--- MainUnit/main/main.ino
+++ MainUnit/main/main.ino
@@ -1,20 +1,33 @@
 #include <SoftwareSerial.h>
 SoftwareSerial rfSerial(2, 3); // RX, TX
-
+#define DEBUG 1
 void setup() {
   // put your setup code here, to run once:
-  Serial.Begin(9600)
+  Serial.begin(9600);
   rfSerial.begin(1200);
-  Serial.println("Waiting "
+ #ifdef DEBUG
+  Serial.println("Waiting for Serial Confirmation...");
   while(!Serial.available())
   {delay(100);}
+  Serial.read();
+  //Serial.println("Wating for an RF Serial Packet Confirmation...");
+  //while(!rfSerial.available())
+  //{ delay(100); }
+ #endif 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int addr; //addr to send to ( 0-4 ) 
   bool type_flag; //Type flag, false for Request true for command
-  //Insert others as needed
+
+  //For Testing...
+  addr=0;
+  for(addr=0;addr<4;addr++)
+  {
+    String Packet = createPacket(addr,type_flag,NULL);
+    Serial.println(Packet);
+  }
 
 
 
