25bda92521a41315dd0092a33450153c8cfae09f
Joseph Morris <jtm245@gmail.com>
2017-05-27 20:14:25 +0100

    Added main score check loop, and placeholders for next steps.

--- MainUnit/main/main.ino
+++ MainUnit/main/main.ino
@@ -10,18 +10,43 @@ void loop() {
   // put your main code here, to run repeatedly:
   int addr; //addr to send to ( 0-4 ) 
   bool type_flag; //Type flag, false for Request true for command
-
   //For Testing...
-  Serial.println("Loop");
-  addr=0;
+  Serial.println("Polling Each Address for Current Score...");
+  type_flag=false; //Request
+  int[4] scoreR;
+  int[4] scoreB;
   for(addr=0;addr<4;addr++)
   {
-    String Packet = createPacket(addr,type_flag,NULL);
-    Serial.println(Packet);
+    String Packet = createPacket(addr,type_flag,byte(1));
+    Serial.println("Sending: "+Packet);
+    rfSerial.println(Packet);
+    //Wait upto 5 Seconds for response.
+    int i = 0;
+    bool rec = false;
+    for(i=0;i<5;i++)
+    {
+      if(rfSerial.available())
+      {
+        rec=true;
+        break;
+      }
+    }
+    if(!rec)
+    {
+      Serial.println("No Packet recieved in 5 Seconds from Addr #" + addr);
+      continue;
+    }
+    else
+    {
+      delay(250);
+      String RecP = String(rfSerial.read());
+      // Plug in the parser here/
+      //scoreR[addr]=rec;
+      //scoreB[addr]=rec;
+    }
   }
-
-
-
+  //TODO: Display Update.
+  //TODO: Check win condition.
 }
 
 
@@ -31,5 +56,6 @@ String createPacket(int addr, bool type, byte response) // Other flags ignored f
   packet.concat(String(addr));
   packet.concat( type == true ? "C":"R");
   packet.concat(String(int(response)));
+  return packet;
 }
 
